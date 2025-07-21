efe95d7d308d1efdebd64dd46ad10efd95e6f026
Diyansh <diyansh@gmail.com>
2025-07-21 21:44:09 +0530

    Add authorization routes and backend functionality

---
+++ backend/controllers/authController.js
@@ -0,0 +1,78 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1h"})
+}
+//Register User
+exports.registerUser = async (req, res) =>{
+    console.log(req.body);
+    const {fullName, email, password, profileImageUrl} = req.body;
+
+    //Validation: Check for missing fields
+    if(!fullName || !email || !password) {
+        return res.status(400).json({message: "You lazy fuck, fill in the fields goddammit"})
+    }
+
+    try{
+        //check if email already exists
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "You want a duplicate of yourself? don't enter an existing email you dumb fuck"})
+        }
+
+        //Create The User
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl
+        });
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    }catch(err){
+        res
+            .status(500)
+            .json({message: "Error Creating User", error: err.message})
+    }
+}
+//Login User
+exports.loginUser = async (req, res) =>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({message: "My god you're so dumb enter the crendentials bruh"});
+    }
+
+    try{
+        const user = await User.findOne({email});
+        if(!user || !(await user.comparePassword(password))){
+            return res.status(400).json({message: "Wrong credentials lmaooo"});
+        }
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id)
+        });
+    }catch(err){
+        return res.status(500).json({message: "Error loggin in the User", error: err.message});
+    }
+};
+//Get User
+exports.getUserInfo = async (req, res) =>{
+    try{
+        const user = await User.findById(req.user.id).select("-password");
+
+        if(!user){
+            return res.status(404).json({message: "User not found"})
+        }
+
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json({message: "Error getting the User", error: err.message})
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes")
 
 const app = express();
 
@@ -17,6 +18,9 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use('/api/v1/auth', authRoutes)
+
 
 connectDB()
 
