0145d8f8479aaf9058f4e48e3d6e9ec3e32df4a3
Diyansh <diyansh@gmail.com>
2025-08-02 16:06:42 +0530

    Craete Dashboard UI, Fetch dashboard data and create summary cards

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
+import SideMenu from './SideMenu';
 
-const Navbar = () => {
+const Navbar = ({activeMenu}) => {
+    const [opensideBar, setOpenSideBar] = useState(true);
   return (
     <div>
-      Navbar
+      <div className='flex gap-5 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-300'>
+        <button 
+            className='block lg:hidden text-black'
+            onClick={() => {
+                setOpenSideBar(!opensideBar); 
+            }}
+        >
+            {opensideBar?(
+                <HiOutlineX className='text-2xl'/>
+            ):(
+                <HiOutlineMenu className='text-2xl'/>
+            )}
+        </button>
+        <h2 className='text-lg font-medium text-black '>Expense Tracker</h2>
+
+        {opensideBar&&(
+            <div className='fixed top-[61px] -ml-4 bg-white'>
+                <SideMenu activeMenu={activeMenu}/>
+            </div>
+        )}
+      </div>
     </div>
   )
 }
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,4 +1,29 @@
 export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
+};
+
+export const getInitials = (name) => {
+    if(!name) return "";
+
+    const word = name.split(" ");
+    let initials = "";
+
+    for(let i = 0 ; i < Math.min(word.length, 2); i++){
+        initials +=  word[i][0];
+    }
+
+    return initials.toUpperCase();
+};
+
+export const addThousandsSeperator = (number) => {
+    if (number == null || isNaN(number)) return "";
+    
+    const [integerPart, fractionPart] = number.toString().split('.');
+
+    const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+    return fractionPart
+        ? `${formattedInteger}.${fractionPart}`
+        : formattedInteger;
 }
